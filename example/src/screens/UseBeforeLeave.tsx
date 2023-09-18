b54b85aa3c9c7e939bd810144a69dba9c7c080f6
Hyesu <honghs95@gmail.com>
2023-09-18 23:52:30 +0900

    remove: delete example directory

--- example/src/screens/UseBeforeLeave.tsx
+++
@@ -1,8 +0,0 @@
-import { useBeforeLeave } from '../hooks/useBeforeLeave';
-
-export default function UseBeforeLeave() {
-	const beofreLeave = useBeforeLeave(() => {
-		console.log('do not leave please!');
-	});
-	return <div></div>;
-}
