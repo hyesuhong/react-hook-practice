936323651ec2e54697b003def1df86a57e336cbd
Hyesu <honghs95@gmail.com>
2023-10-18 15:09:10 +0900

    demo: add style to sidebar

--- example/src/app/components/SideBar.tsx
+++ example/src/app/components/SideBar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
@@ -9,21 +10,41 @@ export default function SideBar() {
 	const pathName = usePathname();
 
 	return (
-		<aside>
-			<h1>su-hooks</h1>
+		<aside className='sticky top-0 grid grid-rows-[max-content_minmax(0,1fr)_min-content] w-[16rem] py-[1rem] border-r border-r-[#eee]'>
+			<h1 className='text-[2.4rem] font-bold px-[1rem] mb-[3rem]'>su-hooks</h1>
 			<ul>
 				{menu.map((item, index) => {
 					const path = `/${item}`;
+					const basicClassName =
+						'h-[4rem] flex items-center justify-between px-[1rem] after:content-[""] after:flex-[0_0_0.6rem] after:h-[0.6rem] after:border-t-2 after:border-r-2 after:border-t-[#333] after:border-r-[#333] after:rotate-45 after:opacity-0 hover:bg-[#eee]/30';
+					const selectedClassName = `${basicClassName} bg-[#eee] after:opacity-100`;
 					return (
-						<li
-							key={index}
-							style={{ background: pathName === path ? 'gold' : '' }}
-						>
-							<Link href={path}>{item}</Link>
+						<li key={index} className='relative text-[1.4rem]'>
+							<Link
+								href={path}
+								className={
+									path === pathName ? selectedClassName : basicClassName
+								}
+							>
+								{item}
+							</Link>
 						</li>
 					);
 				})}
 			</ul>
+			<ol className='flex px-[1rem] items-center'>
+				<li>
+					<a href='https://github.com/hyesuhong/su-hooks' target='_blank'>
+						<Image
+							src='/ico-github.svg'
+							width='32'
+							height='32'
+							alt='github logo'
+							className=''
+						/>
+					</a>
+				</li>
+			</ol>
 		</aside>
 	);
 }
