054d466afd476f8e7534cfa39ab8ba88eaaa8c55
Hyesu <honghs95@gmail.com>
2023-10-20 15:40:22 +0900

    demo: add theme using context api

---
+++ demo/src/contexts/ThemeContext.tsx
@@ -0,0 +1,51 @@
+import {
+	Dispatch,
+	ReactNode,
+	createContext,
+	useContext,
+	useReducer,
+} from 'react';
+
+export type theme = 'light' | 'dark' | 'system';
+
+interface themeProvider {
+	children: ReactNode;
+}
+
+type themeAction = {
+	type: 'CHANGE';
+	nextTheme: theme;
+};
+
+const systemThemeIsDark = matchMedia('prefers-color-scheme: dark');
+const systemTheme = systemThemeIsDark.matches ? 'dark' : 'light';
+
+const ThemeContext = createContext<theme>(systemTheme);
+const ThemeDispatchContext = createContext<Dispatch<themeAction> | null>(null);
+
+const themeReducer = (prevState: theme, action: themeAction) => {
+	console.log(prevState, action);
+	switch (action.type) {
+		case 'CHANGE':
+			return action.nextTheme;
+		default:
+			throw Error('Unknown action: ' + action.type);
+	}
+};
+
+const ThemeProvider = ({ children }: themeProvider) => {
+	const [theme, dispatch] = useReducer(themeReducer, systemTheme);
+
+	return (
+		<ThemeContext.Provider value={theme}>
+			<ThemeDispatchContext.Provider value={dispatch}>
+				{children}
+			</ThemeDispatchContext.Provider>
+		</ThemeContext.Provider>
+	);
+};
+
+export const useTheme = () => useContext(ThemeContext);
+export const useThemeDispatch = () => useContext(ThemeDispatchContext);
+
+export default ThemeProvider;
--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/global.css.ts';
+import ThemeProvider from './contexts/ThemeContext.tsx';
 import MenuProvider from './contexts/MenuContext.tsx';
 
 const listItem = ['useCoords', 'useIntersection', 'useForm'];
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<MenuProvider menu={listItem}>
-			<App />
-		</MenuProvider>
+		<ThemeProvider>
+			<MenuProvider menu={listItem}>
+				<App />
+			</MenuProvider>
+		</ThemeProvider>
 	</React.StrictMode>
 );
