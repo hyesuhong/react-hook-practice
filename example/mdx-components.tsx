957858be22b3a6ff204caf5aeda77a8a159bdd18
Hyesu <honghs95@gmail.com>
2023-11-08 15:59:35 +0900

    demo: add a mdx file

--- example/mdx-components.tsx
+++ example/mdx-components.tsx
@@ -3,5 +3,13 @@ import type { MDXComponents } from 'mdx/types';
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
 		...components,
+		h2: ({ children }) => (
+			<h2 className='text-[2rem] font-medium mb-[4rem]'>{children}</h2>
+		),
+		h3: ({ children }) => (
+			<h3 className='text-[1.8rem] font-medium mt-[2rem] mb-[1rem]'>
+				{children}
+			</h3>
+		),
 	};
 }
--- example/src/app/docs/[hooks]/page.tsx
+++ example/src/app/docs/[hooks]/page.tsx
@@ -1,6 +1,3 @@
-import { promises as fs } from 'fs';
-import { MDXRemote } from 'next-mdx-remote/rsc';
-
 interface props {
 	params: { hooks: string };
 }
@@ -12,27 +9,6 @@ export async function generateMetadata({ params }: props) {
 	};
 }
 
-async function getData(name: string) {
-	const path = `/src/app/_data/${name}.mdx`;
-
-	try {
-		const file = await fs.readFile(process.cwd() + path, 'utf8');
-
-		return file;
-	} catch (e) {
-		console.error(e);
-		throw new Error('Not Found');
-	}
-
-	return '';
-}
-
 export default async function Docs({ params }: props) {
-	const data = await getData(params.hooks);
-
-	return (
-		<>
-			<MDXRemote source={data} />
-		</>
-	);
+	return <>{params.hooks}</>;
 }
