ea3b981f52bdc57c25043e18d5676f57573cbbea
Hyesu <honghs95@gmail.com>
2023-11-23 14:14:09 +0900

    demo: delete mdx

--- example/next.config.mjs
+++ example/next.config.mjs
@@ -1,5 +1,3 @@
-import createMDX from '@next/mdx';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// Configure `pageExtensions`` to include MDX files
@@ -7,13 +5,5 @@ const nextConfig = {
 	// Optionally, add any other Next.js config below
 };
 
-const withMDX = createMDX({
-	// Add markdown plugins here, as desired
-	options: {
-		remarkPlugins: [],
-		rehypePlugins: [],
-	},
-});
-
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
