4c6dd21962143abcc9b9ef4cb15cc425c4e1a68c
Hyesu <honghs95@gmail.com>
2023-11-23 15:49:38 +0900

    demo: fetch contents data in [hooks] route

--- example/src/app/docs/[hooks]/page.tsx
+++ example/src/app/docs/[hooks]/page.tsx
@@ -1,14 +1,26 @@
-interface props {
+import { getDocBySlug } from '@/app/api/docs/getDocs';
+
+interface Props {
 	params: { hooks: string };
 }
 
-export async function generateMetadata({ params }: props) {
-	const hooks = params.hooks;
+export async function generateMetadata({ params }: Props) {
+	const { hooks } = params;
+	const { data } = await getDocBySlug(hooks);
+
 	return {
-		title: hooks,
+		title: data.title,
 	};
 }
 
-export default async function Docs({ params }: props) {
-	return <>{params.hooks}</>;
+export default async function Docs({ params }: Props) {
+	const { hooks } = params;
+	const { data, content } = await getDocBySlug(hooks);
+
+	return (
+		<>
+			<h2 className='text-5xl font-medium mb-40'>{data.title}</h2>
+			<section>{content}</section>
+		</>
+	);
 }
