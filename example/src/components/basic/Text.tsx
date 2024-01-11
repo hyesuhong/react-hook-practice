ab61e2075b415ae87d04fca5b7e7ba3058cd4c91
Hyesu <honghs95@gmail.com>
2024-01-11 19:14:18 +0900

    design: example - add text style

--- example/src/components/basic/Text.tsx
+++ example/src/components/basic/Text.tsx
@@ -1,3 +1,5 @@
+import * as S from '@/styles/text.css';
+
 interface Props {
 	type?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
 	children: React.ReactNode;
@@ -8,46 +10,30 @@ export default function Text({ type = 'p', children, className = '' }: Props) {
 	switch (type) {
 		case 'h1':
 			return (
-				<h1 className={generateClassName([className, 'text-6xl font-bold'])}>
-					{children}
-				</h1>
+				<h1 className={generateClassName([className, S.H1])}>{children}</h1>
 			);
 		case 'h2':
 			return (
-				<h2 className={generateClassName([className, 'text-5xl font-bold'])}>
-					{children}
-				</h2>
+				<h2 className={generateClassName([className, S.H2])}>{children}</h2>
 			);
 		case 'h3':
 			return (
-				<h3 className={generateClassName([className, 'text-4xl font-medium'])}>
-					{children}
-				</h3>
+				<h3 className={generateClassName([className, S.H3])}>{children}</h3>
 			);
 		case 'h4':
 			return (
-				<h4 className={generateClassName([className, 'text-3xl font-medium'])}>
-					{children}
-				</h4>
+				<h4 className={generateClassName([className, S.H4])}>{children}</h4>
 			);
 		case 'h5':
 			return (
-				<h5 className={generateClassName([className, 'text-2xl'])}>
-					{children}
-				</h5>
+				<h5 className={generateClassName([className, S.H5])}>{children}</h5>
 			);
 		case 'h6':
 			return (
-				<h6 className={generateClassName([className, 'text-xl'])}>
-					{children}
-				</h6>
+				<h6 className={generateClassName([className, S.H6])}>{children}</h6>
 			);
 		case 'p':
-			return (
-				<p className={generateClassName([className, 'text-base'])}>
-					{children}
-				</p>
-			);
+			return <p className={generateClassName([className, S.P])}>{children}</p>;
 		case 'span':
 			return <span>{children}</span>;
 	}
